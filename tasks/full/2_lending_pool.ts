8b05bee10c537e5f4847f3db1650c4583731964d
Karim <karim@aurora.dev>
2021-12-02 00:56:53 +0200

    Add goerli support

--- tasks/full/2_lending_pool.ts
+++ tasks/full/2_lending_pool.ts
@@ -77,7 +77,9 @@ task('full:deploy-lending-pool', 'Deploy lending pool for dev enviroment')
       );
       const admin = await DRE.ethers.getSigner(await getEmergencyAdmin(poolConfig));
       // Pause market during deployment
-      await waitForTx(await lendingPoolConfiguratorProxy.connect(admin).setPoolPause(true));
+      await waitForTx(await lendingPoolConfiguratorProxy.connect(admin).setPoolPause(true, {
+        gasLimit: 1600000
+      }));
 
       // Deploy deployment helpers
       await deployStableAndVariableTokensHelper(
